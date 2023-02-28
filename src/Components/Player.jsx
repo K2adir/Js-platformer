f8e0fe275edbef4d45cc055b8300ce94c5f6ae71
K2adir <k2adir@gmail.com>
2023-02-28 11:58:05 -0500

    Player function works - Comment
    try to convet that into Class

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -3,18 +3,22 @@ import React, { useEffect } from "react";
 const Player = ({ c, canvas }) => {
   useEffect(() => {
     let y = 50;
-    let x = 100;
-
+    let x = 50;
+    const height = 50;
     const animate = () => {
+      const bottom = y + height;
+
       window.requestAnimationFrame(animate);
 
       c.clearRect(0, 0, canvas.width, canvas.height);
       c.fillStyle = "white";
       c.fillRect(0, 0, canvas.width, canvas.height);
       c.fillStyle = "red";
-      c.fillRect(x, y, 50, 10);
+      c.fillRect(x, y, 50, height);
 
-      y++;
+      if (bottom < canvas.height) {
+        y++;
+      }
     };
 
     animate();
